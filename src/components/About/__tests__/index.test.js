bd8773f218218e943528bf0bd763555b14b8887b
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2022-02-28 15:33:48 -0600

    installed jest and ran tests for About section
    installations
    @testing-library/jest-dom@5.16.2
    ├── @testing-library/react@12.1.3
    ├── @testing-library/user-event@13.5.0

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+afterEach(cleanup);
+
+describe('About component', () => {
+    //renders About test
+    //First Test
+    it('renders',() => {
+        render(<About/>);
+        
+    });
+    //Second Test
+    it('matches snapshot Dom node structure', () => {
+        //render About
+        const { asFragment } = render(<About/>);
+        expect(asFragment()).toMatchSnapshot();
+    })
+})
