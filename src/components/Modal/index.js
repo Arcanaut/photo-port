2ff0fc306b5055678e71e14b457e9b0232b2d080
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2022-03-08 21:20:16 -0600

    passed images through Modal component
    images now render inside Morals

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
 
-function Modal() {
+function Modal({ onClose, currentPhoto }) {
+  const {name, category, description, index} = currentPhoto;
 
-    return (
-        <div className="modalBackdrop">
-  <div className="modalContainer">
-    <h3 className="modalTitle">Photo Name</h3>
-    <img alt="current category" />
-    <p>
-      Photo Description
-    </p>
-    <button type="button">
-      Close this modal
-    </button>
-  </div>
-</div>
-    );
+  return (
+      <div className="modalBackdrop">
+          <div className="modalContainer">
+              <h3 className="modalTitle">{ name }</h3>
+              <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt="current category" />
+              <p>{ description }</p>
+              <button onClick={onClose} type="button">
+              Close this modal
+              </button>
+          </div>
+      </div>
+  );
 }
 
 export default Modal;
--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -3,7 +3,8 @@ import Modal from '../Modal';
 
 
 const PhotoList = ({ category }) => {
-
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentPhoto, setCurrentPhoto] = useState();
   const [photos] = useState([
     {
       name: 'Grocery aisle',
@@ -104,11 +105,14 @@ const PhotoList = ({ category }) => {
 
   const currentPhotos = photos.filter((photo) => photo.category === category);
   const toggleModal = (image, i) => {
-    // current photo
+    setCurrentPhoto({...image, index: i})
+    setIsModalOpen(!isModalOpen);
   }
+
   return (
     <div>
-      <Modal />
+      {isModalOpen && 
+        <Modal currentPhoto={currentPhoto} onClose={toggleModal} />}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
